6e8398f5373c85fdd3e3925ddb436377a16e1fd8
Miguel <alonsogalalrdomiguel@gmail.com>
2020-04-11 20:03:07 +0200

    coment prueba.js

--- back/bin/prueba.js
+++ back/bin/prueba.js
@@ -1,38 +1,38 @@
-require("dotenv").config();
-const City = require("../models/City");
-const axios = require("axios");
-const {withDbConnection} = require("../lib");
+// require("dotenv").config();
+// const City = require("../models/City");
+// const axios = require("axios");
+// const {withDbConnection} = require("../lib");
 
 
-withDbConnection( async () => {
-  const cities = await City.find();
-  let templeCount = 0;
+// withDbConnection( async () => {
+//   const cities = await City.find();
+//   let templeCount = 0;
 
-  for (city of cities) {
-    try {
-      const response = await axios({
-        url: "https://www.googleapis.com/customsearch/v1",
-        params: {
-          key: process.env.KEY_GOOGLE,
-          cx: process.env.CX_GOOGLE,
-          q: `paris`,
-          searchType: "image",
-          fileType: "jpg",
-          cr: true,
-          alt: "json"
-        }
-      });
-      console.log(response)
-      await City.findByIdAndUpdate(city._id, {
-        museums[0].photo = response.data.items[0].link
-      });
+//   for (city of cities) {
+//     try {
+//       const response = await axios({
+//         url: "https://www.googleapis.com/customsearch/v1",
+//         params: {
+//           key: process.env.KEY_GOOGLE,
+//           cx: process.env.CX_GOOGLE,
+//           q: `paris`,
+//           searchType: "image",
+//           fileType: "jpg",
+//           cr: true,
+//           alt: "json"
+//         }
+//       });
+//       console.log(response)
+//       await City.findByIdAndUpdate(city._id, {
+//         museums[0].photo = response.data.items[0].link
+//       });
 
-    }catch (error) {
-              console.log(
-                error
+//     }catch (error) {
+//               console.log(
+//                 error
                 
-              );
-              break;
-            }
-  }
-});
+//               );
+//               break;
+//             }
+//   }
+// });
