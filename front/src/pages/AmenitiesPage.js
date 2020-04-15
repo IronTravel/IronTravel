793223caf7dd6e87dc0634eca85c369ad5f11877
Carlos Mateo <cmc2k5@gmail.com>
2020-04-15 18:57:17 +0200

    Changes in Amenities

--- front/src/pages/AmenitiesPage.js
+++ front/src/pages/AmenitiesPage.js
@@ -42,44 +42,42 @@ export const AmenitiesPage = () => {
         hobbies().then(res => setHoobies(res.data));
     }, []);
 
-    console.log(hobbiesList)
-
     const handleSetUserHobbies = async (hobbieID) => {
         console.log(hobbieID)
         console.log(userHobbies)
-        let newArr = [...userHobbies, ...[hobbieID]];   
+        let newArr = [...userHobbies, hobbieID];
         console.log(newArr)
         setUserHoobies(newArr);
         await AddHobby(hobbieID);
     }
 
     return (
-      <>
-        <h1>List of Amenities</h1>
-        <div>
-          <ul>
-            {hobbiesList.length &&
-              hobbiesList.map((e, i) => (
-                <li key={i}>
-                  <button onClick={() => handleSetUserHobbies(e._id)}>ADD</button>
-                  {/* <AddHobby idHobby={e._id} added={setUserHoobies} /> */}
-                  {e.name}
-                </li>
-              ))}
-          </ul>
-        
-        <ul>
-        {userHobbies && userHobbies.map((e, i) => (
-              <li key={i}>
-                {/* <DeleteHobby idHobby={e} deleted={fetchHobby1} /> */}
-                {e}
-              </li>
-            ))}
-        </ul>
+        <>
+            <h1>List of Amenities</h1>
+            <div>
+                <ul>
+                    {hobbiesList.length &&
+                        hobbiesList.map((e, i) => (
+                            <li key={i}>
+                                <button onClick={() => handleSetUserHobbies(e._id)}>ADD</button>
+                                {/* <AddHobby idHobby={e._id} added={setUserHoobies} /> */}
+                                {e.name}
+                            </li>
+                        ))}
+                </ul>
+
+                <ul>
+                    {userHobbies && userHobbies.map((e, i) => (
+                        <li key={i}>
+                            {/* <DeleteHobby idHobby={e} deleted={fetchHobby1} /> */}
+                            {e}
+                        </li>
+                    ))}
+                </ul>
 
 
 
-        </div>
-      </>
+            </div>
+        </>
     );
 }
