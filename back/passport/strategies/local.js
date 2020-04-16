7d80e47f403190a968e63c71d301ab132e626cf7
Carlos Mateo <cmc2k5@gmail.com>
2020-04-16 20:12:04 +0200

    fix password checking on login

--- back/passport/strategies/local.js
+++ back/passport/strategies/local.js
@@ -1,24 +1,24 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("../../models/User");
-const { hashPassword } = require("../../lib");
+const { checkHashedPassword } = require("../../lib");
 
 passport.use(
-  new LocalStrategy(async (email, password, done) => {
-    try {
-      if (email === "" || password === "") {
-        done(null, false);
-      }
-      const foundUser = await User.findOne({ email });
-      if (foundUser) {
-        hashPassword(password, foundUser.password)
-          ? done(null, foundUser)
-          : done(null, false);
-      } else {
-        done(null, false);
-      }
-    } catch (error) {
-      done(error);
-    }
-  })
+    new LocalStrategy(async (email, password, done) => {
+        try {
+            if (email === "" || password === "") {
+                done(null, false);
+            }
+            const foundUser = await User.findOne({ email });
+            if (foundUser) {
+                checkHashedPassword(password, foundUser.password)
+                    ? done(null, foundUser)
+                    : done(null, false);
+            } else {
+                done(null, false);
+            }
+        } catch (error) {
+            done(error);
+        }
+    })
 );
