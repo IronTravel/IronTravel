197936236298f97b6700b5fec7629f4975686916
Carlos Mateo <cmc2k5@gmail.com>
2020-04-18 21:27:17 +0200

    Not found + minor css tweaks

--- front/src/App.js
+++ front/src/App.js
@@ -12,6 +12,7 @@ import { MyTravelsPage } from "./pages/MyTravelsPage";
 import { MyToursPage } from './pages/MyToursPage';
 import { MusicHobbiesPage } from './pages/Music-HobbiesPage';
 import { ChangePassword } from "./components/Settings/ChangePassword";
+import { NotFoundPage } from './pages/NotFoundPage';
 import { Google } from "./pages/GoolePage";
 
 // HOCs
@@ -19,25 +20,30 @@ import { withAuth } from './components/withAuthHOC';
 
 export const App = withAuth(() => {
 
-  const slug = window.location.pathname;
+    const slug = window.location.pathname;
 
-  return (
-    <main className={`main-wrapper ${slug === '/auth' ? 'main-wrapper--sm' : 'main-wrapper--lg'}`}>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/auth" exact component={AuthPage} />
-          <Route path="/profile" exact component={ProfilePage} />
-          <Route path="/search" exact component={SearchMatchPage} />
-          <Route path="/my-travels" exact component={MyTravelsPage} />
-          <Route path="/my-tours" exact component={MyToursPage} />
-          <Route path="/settings" exact component={SettingsPage} />
-          <Route path="/settings/personality" exact component={PersonalityPage} />
-          <Route path="/settings/change-password" exact component={ChangePassword} />
-          <Route path="/settings/music-hobbies" exact component={MusicHobbiesPage} />
-          <Route path="/google/login" exact component={Google} />
-        </Switch>
-      </Router>
-    </main>
-  )
+    return (
+        <main className={`main-wrapper ${slug === '/auth' ? 'main-wrapper--sm' : 'main-wrapper--lg'}`}>
+            <Router>
+                <Switch>
+                    {/* <Route path="/" exact component={HomePage} /> */}
+                    <Route path="/auth" exact component={AuthPage} />
+                    <Route path="/profile" exact component={ProfilePage} />
+                    <Route path="/search" exact component={SearchMatchPage} />
+                    <Route path="/my-travels" exact component={MyTravelsPage} />
+                    <Route path="/my-tours" exact component={MyToursPage} />
+                    <Route path="/settings" exact component={SettingsPage} />
+                    <Route path="/settings/personality" exact component={PersonalityPage} />
+                    <Route path="/settings/change-password" exact component={ChangePassword} />
+                    <Route path="/settings/music-hobbies" exact component={MusicHobbiesPage} />
+                    <Route path="/google/login" exact component={Google} />
+
+                    <Route exact path="/" component={() => (
+                        <Redirect to="/auth" />
+                    )} />
+                    <Route path="/" component={NotFoundPage} />
+                </Switch>
+            </Router>
+        </main>
+    )
 });
