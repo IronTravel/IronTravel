dfdcdd8a719db6cc414faf7c0e1d74176117316b
Carlos Mateo <cmc2k5@gmail.com>
2020-04-22 20:38:06 +0200

    Minor fixes

--- front/src/pages/ProfilePage.js
+++ front/src/pages/ProfilePage.js
@@ -16,7 +16,6 @@ import { LikeButton } from '../components/LikeButton';
 import { LikesFaces } from '../components/LikesFaces';
 import { whoami } from '../service/auth';
 
-
 export const ProfilePage = () => {
 
     const loggedInUser = useUser();
@@ -24,27 +23,24 @@ export const ProfilePage = () => {
     const [entries, setEntries] = useState();
     const { id } = useParams();
 
+
     useEffect(() => {
         if (id) {
             getUser(id).then(res => {
-                console.log(res.data)
                 setUser(res.data);
                 getUserEntries(id).then(res => setEntries(res.data));
             });
         } else {
+            //setUser(loggedInUser);
             whoami().then((res) => {
                 setUser(res.data)
                 console.log(res.data._id)
                 // getUserEntries(res.data._id).then(res => setEntries(res.data));
             });
-            console.log(loggedInUser._id)
-            // console.log("holaaa")
-            // console.log(loggedInUser)
-            // setUser(loggedInUser);
             getUserEntries(loggedInUser._id).then(res => setEntries(res.data));
         }
     }, []);
-    
+
     return (
         <>
             <Header />
@@ -133,49 +129,49 @@ export const ProfilePage = () => {
 
                     {/* ProfileAside Component */}
                     <div className="col-3">
-                    
+
                         <section className="section-box">
                             <div className="section-box__header mb-4">
                                 <h2 className="section-box__title px-4">My Travels</h2>
                             </div>
                             <div className="section-box__body px-4">
                                 <div className="image-grid">
-                                {user && user.my_travels.map((e,i) => 
-                                    <div className="image-box" key={i}>
-                                        <a href="#">
-                                            <img src="https://via.placeholder.com/150/150" />
-                                        </a>
-                                    </div>
+                                    {user && user.my_travels.map((e, i) =>
+                                        <div className="image-box" key={i}>
+                                            <a href="#">
+                                                <img src="https://via.placeholder.com/150/150" />
+                                            </a>
+                                        </div>
                                     )}
                                 </div>
                             </div>
                         </section>
-                        
+
                         <section className="section-box">
                             <div className="section-box__header mb-4">
                                 <h2 className="section-box__title px-4">My Tours</h2>
                             </div>
                             <div className="section-box__body px-4">
                                 <article className="tour-small">
-                                {user && user.my_tours.map((e,i) => 
-                                    <a href="#" key={i}>
-                                        <div className="tour-small__img">
-                                            <img src={e.photos} />
-                                        </div>
-                                        <div className="tour-small__info">
-                                            <h2 className="tour-small__info__title">{e.name}</h2>
-                                            <p className="tour-small__info__subtitle">{e.tour_type}</p>
-                                            <div className="tour-small__info__stars"></div>
-                                        </div>
-                                    </a>
-                                    )}
+                                    {
+                                        user && user.my_tours.map((e, i) =>
+                                            <a href="#" key={i}>
+                                                <div className="tour-small__img">
+                                                    <img src={e.photos} />
+                                                </div>
+                                                <div className="tour-small__info">
+                                                    <h2 className="tour-small__info__title">{e.name}</h2>
+                                                    <p className="tour-small__info__subtitle">{e.tour_type}</p>
+                                                    <div className="tour-small__info__stars"></div>
+                                                </div>
+                                            </a>
+                                        )}
                                 </article>
                             </div>
                         </section>
                     </div>
                 </div>
             </div>
-
         </>
     )
 }
